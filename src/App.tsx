f8b8eb9acc867c0555a61ea1b72298af10561339
Roberto <roberto.felipe467@gmail.com>
2025-02-21 17:11:42 -0300

    1° commit - configuração inicial do projeto;

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Button from './components/Button'
+
+function App() {
+
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100" >
+      <Button />
+    </div>
+  )
+}
+
+export default App
